65f22ddef73ca902597f72dc99c35d5a30dc48d3
suzanne su <suhysu@gmail.com>
2018-11-02 22:10:27 -0700

    view page

--- LAB19-CreateAnAPI/Data/TodoItem.cs
+++ LAB19-CreateAnAPI/Data/TodoItem.cs
@@ -7,7 +7,7 @@ namespace LAB19CreateAnAPI.Data
 {
     public class TodoItem
     {
-        public int Id { get; set; }
+        public long Id { get; set; }
 
         public string Name { get; set; }
 

16e1500e376dc68b5ef3af99add2b7aaf6df4719
suzanne su <suhysu@gmail.com>
2018-11-04 16:59:48 -0800

    list name on item

--- LAB19-CreateAnAPI/wwwroot/todo.js
+++ LAB19-CreateAnAPI/wwwroot/todo.js
@@ -14,13 +14,13 @@ function getData() {
             $.each(data, function (key, item) {
                 const checked = item.isComplete ? 'checked' : '';
                 if (item.listId === selectList) {
-                    $('<tr><td><input disabled="true" type="checkbox" ' + checked + '></td>' +
+                    $('<tr><td>' + item.todoList.name + '</td>' +
+                        '<td><input disabled="true" type="checkbox" ' + checked + '></td> ' +
                         '<td>' + item.name + '</td>' +
                         '<td><button onclick="editItem(' + item.id + ')">Edit</button></td>' +
                         '<td><button onclick="deleteItem(' + item.id + ')">Delete</button></td>' +
                         '</tr>').appendTo($('#todos'));
                 }
-                
             });
 
             todos = data;
